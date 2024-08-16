1cd5dcd31782b390660c00eebba67129a74d8997
tolenfra000 <tolenfra000@warren.k12.in.us>
2024-08-16 10:06:35 -0400

    intial commit

--- NICKS.py
+++ NICKS.py
@@ -1,2 +1,7 @@
-num_apples=20
-num_oranges=15
+num_apples = 20
+num_oranges = 15
+print("Number of apples", num_apples)
+print("Number of oranges:",num_oranges)
+num_oranges = 0
+print("Number of apples", num_apples)
+print("Number of oranges:", num_oranges)
